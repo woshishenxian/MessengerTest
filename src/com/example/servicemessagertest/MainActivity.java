e876a544d5e175cbfef4dfc78cadcf010d3a7c1d
vince.liu <vince.liu@kakaocorp.com>
2016-07-18 16:55:57 +0800

    asdf

--- src/com/example/servicemessagertest/MainActivity.java
+++ src/com/example/servicemessagertest/MainActivity.java
@@ -39,7 +39,6 @@ public class MainActivity extends AppCompatActivity {
 			try {
 				mService.send(msg);
 			} catch (RemoteException e) {
-				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
 		}
